9ad7d644ecf8d7652711276bc31958ff0b5861f8
AndersonDev15 <andersondiaz1505@gmail.com>
2026-04-30 14:28:56 -0500

    fix webclient

--- bff/src/main/java/com/barberia/bff/config/WebClientConfig.java
+++ bff/src/main/java/com/barberia/bff/config/WebClientConfig.java
@@ -1,16 +1,28 @@
 package com.barberia.bff.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${app.barberia-api-url}")
+    private String apiBaseUrl;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl(apiBaseUrl)
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .responseTimeout(Duration.ofSeconds(10))
+                ))
                 .build();
     }
 }
