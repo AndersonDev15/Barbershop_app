2b9bd1b2e9849e4178834c0beeb118d2155c21b4
AndersonDev15 <andersondiaz1505@gmail.com>
2026-04-30 09:03:52 -0500

    fix register barbershop

--- server/src/main/java/com/auth/server/Controller/LoginController.java
+++ server/src/main/java/com/auth/server/Controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.auth.server.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";  // sin el slash inicial
-    }
+    @Value("${app.frontend-url}")
+    private String frontendUrl;
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("frontendUrl", frontendUrl);
+        return "login";
+    }
 }
